bf79257b2ff6dc6c936afc594b7a9f8f9d7a1237
YT Huang <38404369+yt-dev@users.noreply.github.com>
2022-02-22 22:49:14 +0800

    update dependencies

--- components/layouts/main.tsx
+++ components/layouts/main.tsx
@@ -1,15 +1,15 @@
 import Head from "next/head"
+import dynamic from "next/dynamic"
 import NavBar from "../navbar"
 import { Box, Container } from "@chakra-ui/react"
 import Footer from "../footer"
+import Loader from "../loader"
 import { Router } from "next/router"
-// import VoxelDogLoader from "../voxel-dog-loader"
-// import dynamic from "next/dynamic"
 
-// const LazyVoxelDog = dynamic(() => import("../voxel-dog"), {
-//   ssr: false,
-//   loading: () => <VoxelDogLoader />,
-// })
+const LazyVoxelDog = dynamic(() => import("../voxel-model"), {
+  ssr: false,
+  loading: () => <Loader />,
+})
 
 interface MainProps {
   router: Router
@@ -19,16 +19,16 @@ const Main: React.FC<MainProps> = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="ytdev's homepage" />
+        <meta name="description" content="yt's homepage" />
         <meta name="author" content="YT Huang" />
-        <meta name="author" content="YT Dev" />
+        <meta name="author" content="yt_dev" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
-        {/* <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" /> */}
-        {/* <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@craftzdog" />
-        <meta name="twitter:creator" content="@craftzdog" />
-        <meta name="twitter:image" content="/card.png" /> */}
-        <meta property="og:site_name" content="ytdev's homepage" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@yt_dev" />
+        <meta name="twitter:creator" content="@yt_dev" />
+        <meta name="twitter:image" content="/card.png" />
+        <meta property="og:site_name" content="YT Huang's Homepage" />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="/card.png" />
         <title>YT Huang - Homepage</title>
@@ -37,8 +37,7 @@ const Main: React.FC<MainProps> = ({ children, router }) => {
       <NavBar path={router.asPath} />
 
       <Container maxW="container.md" pt={14}>
-        {/* <LazyVoxelDog /> */}
-        <Box height={50} />
+        <LazyVoxelDog />
 
         {children}
 
