ec68a6d835faf9e3ae8a1010fe6c77e98a24e509
YT Huang <38404369+yt-dev@users.noreply.github.com>
2022-02-25 21:01:18 +0800

    minor change

--- components/layouts/main.tsx
+++ components/layouts/main.tsx
@@ -11,20 +11,19 @@ const Main: React.FC<MainProps> = ({ children, router }) => {
   return (
     <Box as="main" pb={8}>
       <Head>
+        <title>YT Huang - Homepage</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="ythuang's homepage" />
         <meta name="author" content="YT Huang" />
-        <meta name="author" content="yt_dev" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         {/* <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" /> */}
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@yt_dev" />
-        <meta name="twitter:creator" content="@yt_dev" />
-        <meta name="twitter:image" content="/card.png" />
+        <meta name="twitter:site" content="@kuon_one" />
+        <meta name="twitter:creator" content="@kuon_one" />
+        <meta name="twitter:image" content="/thumbnail.png" />
         <meta property="og:site_name" content="YT Huang's Homepage" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="/card.png" />
-        <title>YT Huang - Homepage</title>
+        <meta property="og:image" content="/thumbnail.png" />
       </Head>
 
       <NavBar path={router.asPath} />
